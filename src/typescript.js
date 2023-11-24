d8d24fdfe590cb47af86125bf423b6bc8d177dbd
Richard Martins <richardmartins.me@gmail.com>
2023-11-24 19:16:53 -0300

    fixes

--- src/typescript.js
+++ src/typescript.js
@@ -48,17 +48,12 @@ module.exports = {
     "@typescript-eslint/no-non-null-assertion": "error",
     "@typescript-eslint/no-non-null-asserted-nullish-coalescing": "error",
     "@typescript-eslint/no-mixed-enums": "error",
-    "@typescript-eslint/no-misused-promises": "error",
-    "@typescript-eslint/no-extraneous-class": "error",
     "@typescript-eslint/no-unused-vars": ["error", {
       "argsIgnorePattern": "^_",
       "varsIgnorePattern": "^_",
       "caughtErrorsIgnorePattern": "^_",
       "destructuredArrayIgnorePattern": "^_",
     }],
-    "@typescript-eslint/member-ordering": [
-      "error",
-      { "classExpressions": ["field", "constructor", "get", "method", "private-method", "static-method"] }
-    ],
+    "@typescript-eslint/member-ordering": "error",
   },
 };
